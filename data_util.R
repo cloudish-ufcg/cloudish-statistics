2754417ebdb03d2e5cd0e96b1e835c0c2c76a5df
Fabio Morais <fabiomorais@copin.ufcg.edu.br>
2017-03-13 13:56:38 -0300

    Specific functions to read from database

--- data_util.R
+++ data_util.R
@@ -11,3 +11,27 @@ LoadDatabaseData <- function(filename, tablename) {
 WriteData <- function(data.result, filename) {
      write.table(data.result, filename, quote = F, row.names = F)
 }
+
+GetTaskData <- function(input.dir.base, datacenter.type, simulation.date, preemption.policy) {
+     input.file.tasks <- paste(input.dir.base, "/googletasks-", datacenter.type, "-", simulation.date, "-", preemption.policy, "-epoch1s.sqlite3", sep = "")
+     tasks <- LoadDatabaseData(input.file.tasks, "googletasks")
+     return(tasks)
+}
+
+GetCheckpointData <- function(input.dir.base, input.file.checkpoint) {
+     input.file.checkpoint <- paste(input.dir.base, "/", input.file.checkpoint, sep = "")
+     checkpoints <- LoadDatabaseData(input.file.checkpoint, "preemptivevms")
+     return(checkpoints)
+}
+
+GetDatacenterData <- function(input.dir.base, datacenter.type, simulation.date, preemption.policy) {
+     input.file.datacenter <- paste(input.dir.base, "/datacenter-", datacenter.type, "-", simulation.date, "-", preemption.policy, "-epoch1s.sqlite3", sep = "")
+     datacenter <- LoadDatabaseData(input.file.datacenter, "datacenterusage")
+     return(datacenter)
+}
+
+GetUsageData <- function(input.dir.base, datacenter.type, simulation.date, preemption.policy) {
+     input.file.utilization <- paste(input.dir.base, "/utilization-", datacenter.type, "-", simulation.date, "-", preemption.policy, "-epoch1s.sqlite3", sep = "")
+     usage <- LoadDatabaseData(input.file.utilization, "usage")
+     return(usage)
+}
